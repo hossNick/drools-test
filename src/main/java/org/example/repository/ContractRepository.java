a78d8bb4f48af5181945e98744d71e5e1223a1a8
hoss <nickirita461@gmail.com>
2025-06-30 10:52:01 +0330

    add repositories

---
+++ src/main/java/org/example/repository/ContractRepository.java
@@ -0,0 +1,10 @@
+package org.example.repository;
+
+import org.example.entities.Contract;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+import java.util.UUID;
+
+public interface ContractRepository extends JpaRepository<Contract, UUID>, JpaSpecificationExecutor<Contract> {
+}
